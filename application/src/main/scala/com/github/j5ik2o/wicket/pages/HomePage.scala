ecebc8a201350bc7fdc4ebedec28223636a6e715
Junichi Kato <j5ik2o@gmail.com>
2015-06-12 10:21:22 +0900

    optimize imports

--- application/src/main/scala/com/github/j5ik2o/wicket/pages/HomePage.scala
+++ application/src/main/scala/com/github/j5ik2o/wicket/pages/HomePage.scala
@@ -1,9 +1,5 @@
 package com.github.j5ik2o.wicket.pages
 
-import org.apache.wicket.markup.html.WebPage
-import org.apache.wicket.markup.html.basic.Label
-import org.apache.wicket.markup.html.link.Link
-
 class HomePage extends BasePage {
 
 }
